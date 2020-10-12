9ca43e4fffeffe6d886a7308c25cca1a47119b15
meizikeai <meizikeai@163.com>
2020-10-12 17:41:05 +0800

    fix: update

--- server/libs/connect.js
+++ server/libs/connect.js
@@ -1,5 +1,6 @@
 const createMySQLClient = require('ai-mysql-client')
 const createRedisClient = require('ai-redis-client')
+const createQconfClient = require('ai-qconf-client')
 const { Qconf } = require('@blued-core/qconf')
 
 const datum = require('../config/datum')
@@ -14,7 +15,8 @@ const mysqlClient = key => {
   }
 
   return createMySQLClient({
-    key, option: qconf,
+    key,
+    option: qconf,
   })()
 }
 
@@ -24,11 +26,51 @@ const redisClient = key => {
   }
 
   return createRedisClient({
-    key, option: qconf,
+    key,
+    option: qconf,
   })()
 }
 
-module.exports = {
+const getQconfHost = key => {
+  if (!(key in datum)) {
+    throw new Error(`Can not find the key: [${key}]`)
+  }
+
+  return createQconfClient.qconfHost({
+    key,
+    path: datum[key].qconf,
+    option: qconf,
+  })()
+}
+
+const getQconfAllHost = key => {
+  if (!(key in datum)) {
+    throw new Error(`Can not find the key: [${key}]`)
+  }
+
+  return createQconfClient.qconfAllHost({
+    key,
+    path: datum[key].qconf,
+    option: qconf,
+  })()
+}
+
+const getQconfConf = key => {
+  if (!(key in datum)) {
+    throw new Error(`Can not find the key: [${key}]`)
+  }
+
+  return createQconfClient.qconfConf({
+    key,
+    path: datum[key].qconf,
+    option: qconf,
+  })()
+}
+
+export {
+  getQconfAllHost,
+  getQconfConf,
+  getQconfHost,
   mysqlClient,
   redisClient,
 }
