e6dbf9dc8de22e0c8c49ffbbf940ea1a6460df8a
meizikeai <meizikeai@163.com>
2020-11-27 10:48:13 +0800

    feat: update

---
+++ server/libs/request.js
@@ -0,0 +1,56 @@
+import fetch from 'node-fetch'
+import crypto from 'crypto'
+import logger from './logger'
+import { getQconfHost } from './connect'
+
+async function request({
+  url,
+  data,
+  headers = {},
+  method = 'get',
+  qconf,
+  reject,
+  resolve,
+}) {
+  let request = ''
+
+  if (/^http/ig.test(url)) {
+    request = url
+  } else {
+    const protocol = getQconfHost(qconf)
+    request = `http://${protocol.host}:${protocol.port}${url}`
+  }
+
+  const option = {
+    method,
+    headers: Object.assign({
+      'Content-Type': 'application/json',
+      'X-Request-ID': crypto.randomBytes(16).toString('hex'),
+    }, headers),
+  }
+
+  if (method.toLocaleLowerCase() === 'post') {
+    option.body = JSON.stringify(data || {})
+  }
+
+  const result = await fetch(request, option)
+    .then(res => res.json())
+    .then(res => {
+      if (typeof resolve === 'function') {
+        return resolve(res)
+      }
+
+      logger.info({ url: request, option, result: res })
+    })
+    .catch(error => {
+      if (typeof reject === 'function') {
+        reject(error)
+      }
+
+      logger.error(error, { url: request, option })
+    })
+
+  return result
+}
+
+export default request
