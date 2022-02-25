5c7fd035fe5c8ba764e37ece1a24568838d8df3c
meizikeai <meizikeai@163.com>
2022-02-25 11:49:55 +0800

    update

--- server/config/env.js
+++ server/config/env.js
@@ -11,4 +11,4 @@ module.exports.isLocalPro = dev === 'localPro'
 // zookeeper
 module.exports.useZookeeper = true
 module.exports.release = ['10.10.182.158:2181', '10.10.101.151:2181', '10.10.102.25:2181']
-module.exports.test = ['10.9.158.210:2181', '10.9.114.167:2181', '10.9.105.4:2181']
+module.exports.test = ['10.9.158.210:2181']
