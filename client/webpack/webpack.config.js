0599ded2ad413ff5b6577a8d939ab35722deb410
meizikeai <meizikeai@163.com>
2023-09-08 16:41:24 +0800

    update

--- client/webpack/webpack.config.js
+++ client/webpack/webpack.config.js
@@ -172,7 +172,7 @@ module.exports = (env, argv) => {
   })
 
   if (argv.mode === 'production') {
-    config.devtool = 'none'
+    config.devtool = false
     // config.output.publicPath = `${cdn}/web/static/`
     config.optimization.minimize = true
     config.plugins.push(compressionPlugin)
