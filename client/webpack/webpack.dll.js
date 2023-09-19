766df36806c80ae8719182101e4e6a1abe58fc7d
meizikeai <meizikeai@163.com>
2023-09-19 14:26:52 +0800

    update

--- client/webpack/webpack.dll.js
+++ client/webpack/webpack.dll.js
@@ -4,7 +4,7 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  devtool: 'none',
+  devtool: false,
   entry: {
     vendors: ['react', 'react-dom', 'react-router', 'react-router-dom', 'prop-types', 'whatwg-fetch'],
   },
